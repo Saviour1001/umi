b527c434b8a2dac394abf38af123ff8687fea9b0
Saviour1001 <harshghodkar@gmail.com>
2024-04-08 22:55:18 +0530

    push

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import {
   createSignerFromKeypair,
   generateSigner,
+  PublicKey,
   signerIdentity,
 } from "@metaplex-foundation/umi";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { createV1 } from "@metaplex-foundation/mpl-core";
+import { createV1, createCollectionV1 } from "@metaplex-foundation/mpl-core";
 import { secretKey } from "./consts";
 
 const umi = createUmi(
@@ -20,11 +21,35 @@ const myKeypairSigner = createSignerFromKeypair(umi, myKeypair);
 
 umi.use(signerIdentity(myKeypairSigner));
 
-const asset = generateSigner(umi);
-const result = await createV1(umi, {
-  asset: asset,
-  name: "My Nft",
-  uri: "https://ipfs.moralis.io:2053/ipfs/QmWHnPjenBcPpjrjZ5yrRfHyJ5nrNGMQpRyucL3F8Wgd9m",
-});
+// create a function for creating a collection
+async function createCollection() {
+  const collectionSigner = generateSigner(umi);
+  console.log("Collection Address", collectionSigner.publicKey.toString());
+  await createCollectionV1(umi, {
+    collection: collectionSigner,
+    name: "My Collection",
+    uri: "https://ipfs.moralis.io:2053/ipfs/QmWHnPjenBcPpjrjZ5yrRfHyJ5nrNGMQpRyucL3F8Wgd9m",
+  }).sendAndConfirm(umi);
+}
 
-console.log(result);
+// createCollection();
+
+async function createNFTOfCollection(
+  collectionAddress: string,
+  newOwner: string
+) {
+  const asset = generateSigner(umi);
+  const result = await createV1(umi, {
+    asset: asset,
+    name: "My Nft",
+    uri: "https://ipfs.moralis.io:2053/ipfs/QmWHnPjenBcPpjrjZ5yrRfHyJ5nrNGMQpRyucL3F8Wgd9m",
+    collection: collectionAddress as PublicKey,
+    owner: newOwner as PublicKey,
+  }).sendAndConfirm(umi);
+  console.log("NFT", result);
+}
+
+// createNFTOfCollection(
+//   "88gBicCpxVJQt6hvmqyaMyMz9whNbVVLqEysaLgvchN8", // collection address
+//   "BGfybQ2uFGPmCscCPAgJtBFXDWc5GNqzymSq3AAo6Nvi" // user address
+// );
